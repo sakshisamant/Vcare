84c8a457ad3984cbee2603d71da927e5398e148b
Sakshi Samant <sakshisamant79@gmail.com>
2018-07-21 14:49:44 +0530

    Initial commit

---
+++ app/src/main/java/sakshi/com/vcare/Patient_Form_Activity.java
@@ -0,0 +1,16 @@
+package sakshi.com.vcare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Patient_Form_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient__form_);
+
+
+
+    }
+}
